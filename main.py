78e003a5f56f1a826fc4b5761e9edc87eb22aebc
Sonia-Mo <sonia.moushaev@mail.huji.ac.il>
2022-07-03 12:25:13 +0300

    Create routers directory
    * Created new router for todos and moved todo's CRUD functions to routers directory

--- main.py
+++ main.py
@@ -1,61 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from models import BasicTodo, ShowTodo
+from routers import todos
 
 app = FastAPI()
 
-memory: list[ShowTodo] = []
-
-
-# =========================== CRUD Methods on todos ===========================
-
-# Get all todos
-@app.get("/", response_model=list[ShowTodo])
-async def get_all_todos():
-    return memory
-
-
-# Get single to do
-@app.get("/{todo_id}", response_model=ShowTodo)
-async def get_todo(todo_id: int):
-    for elem in memory:
-        if elem.todo_id == todo_id:
-            return elem
-    raise HTTPException(
-        status_code=404,
-        detail=f"item number {todo_id} was not found"
-    )
-
-
-# Create new to do
-@app.post("/{todo_id}", response_model=ShowTodo)
-async def create_todo(user_todo: BasicTodo):
-    next_id = len(memory) + 1
-    todo = ShowTodo(todo_id=next_id, **user_todo.dict())
-    memory.append(todo)
-    return todo
-
-
-# Delete to do
-@app.delete("/{todo_id}")
-async def delete_todo(todo_id: int):
-    for elem in memory:
-        if elem.todo_id == todo_id:
-            memory.remove(elem)
-            return
-    raise HTTPException(
-        status_code=404,
-        detail=f"item number {todo_id} was not found"
-    )
-
-
-# Update completion of existing to do
-@app.patch("/{todo_id}", response_model=ShowTodo)
-async def update_todo_completion(todo_id: int, completed: bool = True):
-    for elem in memory:
-        if elem.todo_id == todo_id:
-            elem.completion = completed
-            return elem
-    raise HTTPException(
-        status_code=404,
-        detail=f"item number {todo_id} was not found"
-    )
+app.include_router(todos.router)
---
+++ routers/todos.py
@@ -0,0 +1,62 @@
+from fastapi import APIRouter, HTTPException
+from ..models import BasicTodo, ShowTodo
+
+router = APIRouter(
+    prefix="/todos",
+    tags=["todos"],
+)
+
+memory: list[ShowTodo] = []
+
+
+# Get all todos
+@router.get("/", response_model=list[ShowTodo])
+async def get_all_todos():
+    return memory
+
+
+# Get single to do
+@router.get("/{todo_id}", response_model=ShowTodo)
+async def get_todo(todo_id: int):
+    for elem in memory:
+        if elem.todo_id == todo_id:
+            return elem
+    raise HTTPException(
+        status_code=404,
+        detail=f"item number {todo_id} was not found"
+    )
+
+
+# Create new to do
+@router.post("/{todo_id}", response_model=ShowTodo)
+async def create_todo(user_todo: BasicTodo):
+    next_id = len(memory) + 1
+    todo = ShowTodo(todo_id=next_id, **user_todo.dict())
+    memory.append(todo)
+    return todo
+
+
+# Delete to do
+@router.delete("/{todo_id}")
+async def delete_todo(todo_id: int):
+    for elem in memory:
+        if elem.todo_id == todo_id:
+            memory.remove(elem)
+            return
+    raise HTTPException(
+        status_code=404,
+        detail=f"item number {todo_id} was not found"
+    )
+
+
+# Update completion of existing to do
+@router.patch("/{todo_id}", response_model=ShowTodo)
+async def update_todo_completion(todo_id: int, completed: bool = True):
+    for elem in memory:
+        if elem.todo_id == todo_id:
+            elem.completion = completed
+            return elem
+    raise HTTPException(
+        status_code=404,
+        detail=f"item number {todo_id} was not found"
+    )
