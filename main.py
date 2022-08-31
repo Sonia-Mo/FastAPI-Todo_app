95c38ee3ba0d624602ef29a7951ef1b47ed04595
Sonia-Mo <sonia.moushaev@mail.huji.ac.il>
2022-08-31 18:58:06 +0300

    Added endpoint for login

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI
-from database import engine
+from datetime import timedelta
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from JWTtoken import Token, create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
+from authentication import authenticate_user
+from database import engine, get_db
 import models
 from routers.todos import todos
 from routers.users import users
@@ -10,3 +15,19 @@ app = FastAPI()
 
 app.include_router(todos.router)
 app.include_router(users.router)
+
+
+@app.post("/token", response_model=Token, include_in_schema=False)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
